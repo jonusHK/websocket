5978ffb633a98c534666a1b15342ce6efe74b5dc
ethan <ethan@monolabs.io>
2022-04-26 22:57:11 +0900

    Add: fastapi & template 구조 추가

---
+++ server/main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from starlette.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
