9e64b8e204eb16e9fc632a33a37619d73d1cacf6
ethan <ethan@monolabs.io>
2022-04-27 22:55:25 +0900

    Add: 웹소켓 서버 구현

--- server/main.py
+++ server/main.py
@@ -1,8 +1,14 @@
+import asyncio
+import typing
+from time import time
+
 import uvicorn
 from fastapi import FastAPI, Request
+from starlette.endpoints import WebSocketEndpoint
 from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
+from starlette.websockets import WebSocket
 
 app = FastAPI()
 
@@ -14,6 +20,31 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+# @app.websocket("/chat")
+# async def chat(websocket: WebSocket):
+#     await websocket.accept()
+#     while True:
+#         data = await websocket.receive_text()
+#         await websocket.send_text(f"Message: {data}")
+
+
+@app.websocket_route("/chat")
+class WebSocketChat(WebSocketEndpoint):
+    async def on_connect(self, websocket):
+        await websocket.accept()
+        self.last_time = time()
+        print(f"[{self.last_time}] connected: {websocket.client}")
+
+    async def on_receive(self, websocket: WebSocket, data) -> None:
+        await websocket.send_text(data)
+        self.last_time = time()
+        print(f"[{self.last_time}] {data}")
+
+    async def on_disconnect(self, websocket, close_code):
+        print(f"[{time()}] disconnected: {websocket.client}")
+        print("delay:", time() - self.last_time)
 
 
 if __name__ == "__main__":
