7f678b5a2766deebf4d2a281471a4195623e68bd
jonus <positipman@gmail.com>
2022-08-16 01:43:16 +0900

    Fix: reponse data 없을 시, data 제거

--- server/core/responses.py
+++ server/core/responses.py
@@ -8,7 +8,8 @@ from server.core.enums import ResponseCode
 class WebsocketJSONResponse(JSONResponse):
     def render(self, content: typing.Union[list, dict]) -> bytes:
         response = ResponseCode.OK.retrieve()
-        response.update({'data': content})
+        if content:
+            response.update({'data': content})
 
         if isinstance(content, list):
             response.update({'total': len(content)})
