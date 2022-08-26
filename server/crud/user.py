f96aba1fad48f638f4ba566acb3eb5b301b11fd0
jonus <positipman@gmail.com>
2022-08-26 21:40:21 +0900

    Fix: fix

--- server/crud/user.py
+++ server/crud/user.py
@@ -2,42 +2,42 @@ from sqlalchemy import insert, select, update, delete
 from sqlalchemy.orm import Session
 
 from server.core.utils import hash_password
-from server.models import user as user_models
-from server.schemas import user as user_schemas
+from server.models.user import User, UserSession
+from server.schemas.user import UserCreate, UserSessionCreate
 
 
 def get_user_by_uid(db: Session, uid: str):
-    return db.query(user_models.User).filter(user_models.User.uid == uid).first()
+    return db.query(User).filter(User.uid == uid).first()
 
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(user_models.User).filter(user_models.User.email == email).first()
+    return db.query(User).filter(User.email == email).first()
 
 
-def create_user(db: Session, user: user_schemas.UserCreate):
+def create_user(db: Session, user: UserCreate):
     user_dict = user.dict()
     user_dict.update({
         'password': hash_password(user.password)
     })
-    db_user = user_models.User(**user_dict)
+    db_user = User(**user_dict)
     db.add(db_user)
     return db_user
 
 
 def update_user(db: Session, target_id: int, **kwargs):
     stmt = (
-        update(user_models.User).
-        where(user_models.User.id == target_id).
+        update(User).
+        where(User.id == target_id).
         values(**kwargs).
         execution_options(synchronize_session="fetch")
     )
     db.execute(stmt)
 
 
-def create_session(db: Session, user_session: user_schemas.UserSessionCreate):
+def create_session(db: Session, user_session: UserSessionCreate):
     user_session_dict = user_session.dict()
     stmt = (
-        insert(user_models.UserSession).
+        insert(UserSession).
         values(**user_session_dict)
     )
     db.execute(stmt)
@@ -45,7 +45,7 @@ def create_session(db: Session, user_session: user_schemas.UserSessionCreate):
 
 def get_session_by_session_id(db: Session, session_id: str):
     stmt = (
-        select(user_models.UserSession).
+        select(UserSession).
         filter_by(session_id=session_id).
         limit(1)
     )
@@ -54,8 +54,8 @@ def get_session_by_session_id(db: Session, session_id: str):
 
 def update_session(db: Session, target_id: int, **kwargs):
     stmt = (
-        update(user_models.UserSession).
-        where(user_models.UserSession.id == target_id).
+        update(UserSession).
+        where(UserSession.id == target_id).
         values(**kwargs).
         execution_options(synchronize_session="fetch")
     )
@@ -64,8 +64,8 @@ def update_session(db: Session, target_id: int, **kwargs):
 
 def delete_session(db: Session, target_id: int):
     stmt = (
-        delete(user_models.UserSession).
-        where(user_models.UserSession.id == target_id).
+        delete(UserSession).
+        where(UserSession.id == target_id).
         execution_options(synchronize_session="fetch")
     )
     db.execute(stmt)
