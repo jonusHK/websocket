b0a9e141a970c6dad683cef649967e0ccca1a0ae
jonus <positipman@gmail.com>
2022-08-27 13:08:12 +0900

    Fix: 서버 재시작 시, db drop 되는 버그 수정

--- server/db/databases.py
+++ server/db/databases.py
@@ -16,10 +16,10 @@ DATABASE_URL = "mysql+asyncmy://{username}:{password}@{host}:{port}/{db_name}".f
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
 Base = declarative_base()
+# sessionmaker : 엔진의 연결 풀을 요청하고, 새로운 세션 객체와 연결하여, 새로운 세션 객체를 초기화하는 자동화 기능을 수행함
+async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    # sessionmaker : 엔진의 연결 풀을 요청하고, 새로운 세션 객체와 연결하여, 새로운 세션 객체를 초기화하는 자동화 기능을 수행함
-    async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
     async with async_session() as session:
         yield session
--- server/main.py
+++ server/main.py
@@ -36,7 +36,6 @@ templates = Jinja2Templates(directory="templates")
 async def startup():
     # create db tables
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
