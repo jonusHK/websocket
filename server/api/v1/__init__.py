ce034328909c853e011c772039a5b03b78c26351
jonus <positipman@gmail.com>
2022-11-03 17:41:58 +0900

    Move: chat websocket 경로 변경

--- server/api/v1/__init__.py
+++ server/api/v1/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from server.api.v1 import base, service, user
+from server.api.v1 import base, service, user, chat
 
 api_router = APIRouter()
 api_router.include_router(base.router)
 api_router.include_router(service.router, prefix="/services")
 api_router.include_router(user.router, prefix="/users")
+api_router.include_router(chat.router, prefix="/chats")
--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List, Mapping
+from typing import List, Dict
 
 import uvicorn
 from fastapi import FastAPI, Request
@@ -34,8 +34,6 @@ app.include_router(api_router, prefix=settings.api_v1_prefix)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
-
 
 @app.on_event("startup")
 async def on_startup():
@@ -58,51 +56,6 @@ async def exception_handler(request: Request, exc: ClassifiableException):
         "content": jsonable_encoder(err_response)
     }
     return JSONResponse(**kwargs)
-
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: Mapping[str, List[WebSocket]] = {}
-
-    async def connect(self, websocket: WebSocket, room_id: str):
-        await websocket.accept()
-        connections_for_room = self.active_connections.get(room_id, [])
-        connections_for_room.append(websocket)
-        self.active_connections[room_id] = connections_for_room
-
-    def disconnect(self, websocket: WebSocket, room_id: str):
-        if websocket in self.active_connections[room_id]:
-            self.active_connections[room_id].remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: str, room_id: str):
-        for connection in self.active_connections[room_id]:
-            await connection.send_text(message)
-
-
-manager = ConnectionManager()
-
-
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.websocket("/chat/{room_id}/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, room_id: str, client_id: str):
-    await manager.connect(websocket, room_id)
-    # TODO : DB 에서 room_id & client_id 매핑 여부 확인 -> 매핑되어 있지 않을 시 브로드캐스트로 입장 메시지 송신
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await manager.broadcast(f"Client #{client_id} says: {data}", room_id)
-    except WebSocketDisconnect as e:
-        # 반드시 웹소켓 연결 해제 후, 브로드캐스트 진행
-        manager.disconnect(websocket, room_id)
-        if e.code == CLIENT_DISCONNECT:
-            await manager.broadcast(f"Client #{client_id} left the chat", room_id)
 
 
 # @app.websocket_route("/chat")
