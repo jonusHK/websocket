6516f346d1c447970963eea48d293ccfd29c7e0e
jonus <positipman@gmail.com>
2022-11-09 00:39:13 +0900

    Add: 채팅 레이아웃 컴포넌트 설정

---
+++ client/src/components/MainLayer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <div>
+        <user-list-layer/>
+        <room-title-layer/>
+        <chat-list-layer/>
+        <chat-input-layer/>
+      </div>
+      <div>
+        <user-profile-layer/>
+      </div>
+    </div>
+</template>
+
+<script>
+import RoomTitleLayer from './RoomTitleLayer.vue';
+import ChatListLayer from './ChatListLayer.vue';
+import ChatInputLayer from './ChatInputLayer.vue';
+import UserProfileLayer from './UserProfileLayer.vue';
+import UserListLayer from './UserListLayer.vue';
+
+export default {
+  name: 'MainLayer.vue',
+  components: {
+    RoomTitleLayer,
+    ChatListLayer,
+    ChatInputLayer,
+    UserProfileLayer,
+    UserListLayer,
+  }
+}
+</script>
