94bde2001695b1def4a52916a6bb169b5827e58f
jonus <positipman@gmail.com>
2022-11-22 23:23:49 +0900

    Fix: fix

--- server/api/v1/user.py
+++ server/api/v1/user.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from uuid import uuid4, UUID
 
 from fastapi import APIRouter, Depends, HTTPException, Response, status
-from fastapi.encoders import jsonable_encoder
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from server.api import ExceptionHandlerRoute
@@ -11,19 +10,19 @@ from server.core.enums import UserType
 from server.core.utils import verify_password
 from server.crud.user import UserCRUD, UserProfileCRUD
 from server.db.databases import get_async_session
-from server.models import user as user_models
-from server.schemas import user as user_schemas
+from server.models import UserSession
+from server.schemas.user import UserS, UserSessionS, UserCreateS
 
 router = APIRouter(route_class=ExceptionHandlerRoute)
 
 
 @router.post(
     "/signup",
-    response_model=user_schemas.User,
+    response_model=UserS,
     response_model_include={"id", "uid"},
     status_code=status.HTTP_201_CREATED
 )
-async def signup(user_s: user_schemas.UserCreate, session: AsyncSession = Depends(get_async_session)):
+async def signup(user_s: UserCreateS, session: AsyncSession = Depends(get_async_session)):
     user_crud = UserCRUD(session)
     user_profile_crud = UserProfileCRUD(session)
 
@@ -35,12 +34,12 @@ async def signup(user_s: user_schemas.UserCreate, session: AsyncSession = Depend
     await user_profile_crud.create(user=user, nickname=user.name, is_default=True)
     await session.commit()
 
-    return user_schemas.User.from_orm(user)
+    return UserS.from_orm(user)
 
 
 @router.post(
     "/login",
-    response_model=user_schemas.User,
+    response_model=UserS,
     response_model_include={"id"}
 )
 async def login(data: SessionData, response: Response, session: AsyncSession = Depends(get_async_session)):
@@ -52,15 +51,15 @@ async def login(data: SessionData, response: Response, session: AsyncSession = D
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid password.")
 
     await backend.create(session_id, data, session)
-    cookie.attach_to_response(response, session_id)
+    cookie.attach_to_response(response, session_id)  # 저장 되는 쿠키 값: str(cookie.signer.dumps(session_id.hex))
     await UserCRUD(session).update_user(user.id, last_login=datetime.now().astimezone())
     await session.commit()
-    return user_schemas.User.from_orm(user)
+    return UserS.from_orm(user)
 
 
 @router.get("/whoami", dependencies=[Depends(cookie)])
-async def whoami(user_session: user_models.UserSession = Depends(verifier)):
-    return user_schemas.UserSession.from_orm(user_session)
+async def whoami(user_session: UserSession = Depends(verifier)):
+    return UserSessionS.from_orm(user_session)
 
 
 @router.get("/permission", dependencies=[Depends(cookie), Depends(RoleChecker([UserType.USER]))])
