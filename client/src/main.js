c389efe5d716c26046858a3275ab23b810e054c5
jonus <positipman@gmail.com>
2022-12-20 23:42:49 +0900

    Add: axios, router, store 글로벌 변수 설정

--- client/src/main.js
+++ client/src/main.js
@@ -7,8 +7,15 @@ import BalmUIPlus from 'balm-ui-plus'; // BalmJS Team Material Components
 import 'balm-ui-css';
 import VueResizeObserver from 'vue-resize-observer';
 import jQuery from 'jquery';
+import axios from 'axios'
+import './assets/main.css';
 
 const app = createApp(App);
+
+axios.defaults.withCredentials = true;
+app.config.globalProperties.$axios = axios;
+app.config.globalProperties.$router = router;
+app.config.globalProperties.$store = store;
 
 app.use(store);
 app.use(router);
