6f5f8f2d27db7cead2101e75b99f2d9a7fe2b0e6
jonus <positipman@gmail.com>
2022-12-20 23:41:10 +0900

    Add: vite config 수정 (환경변수 설정 외)

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  define: {
-    global: {},
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      vue: 'vue/dist/vue.esm-bundler.js',
-      'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.esm.js',
-      'balm-ui-css': 'balm-ui/dist/balm-ui.css',
-      'balm-ui-source': 'balm-ui/src/scripts/balm-ui.js',
-      'balm-ui-plus-source': 'balm-ui/src/scripts/balm-ui-plus.js'
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [vue()],
+    define: {
+      __APP_ENV__: env.APP_ENV,
+      global: {},
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        vue: 'vue/dist/vue.esm-bundler.js',
+        'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.esm.js',
+        'balm-ui-css': 'balm-ui/dist/balm-ui.css',
+        'balm-ui-source': 'balm-ui/src/scripts/balm-ui.js',
+        'balm-ui-plus-source': 'balm-ui/src/scripts/balm-ui-plus.js'
+      }
     }
   }
 })
