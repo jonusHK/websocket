9e8d494c82ad79f55967e5610864979e4dad5a9e
jonus <positipman@gmail.com>
2022-12-22 00:22:06 +0900

    Add: 회원가입 시, 모바일 번호를 유효한 포맷으로 변경하여 저장

--- server/core/utils/__init__.py
+++ server/core/utils/__init__.py
@@ -1,7 +1,9 @@
 from passlib.context import CryptContext
-from pytz import timezone
 from sqlalchemy import Integer
 from sqlalchemy.types import TypeDecorator
+import phonenumbers
+from phonenumbers.phonenumber import PhoneNumber as BasePhoneNumber
+from sqlalchemy_utils import PhoneNumber, PhoneNumberParseException
 
 
 class IntTypeEnum(TypeDecorator):
@@ -35,3 +37,54 @@ def hash_password(password: str) -> bytes:
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def validate_region(region):
+    if (
+        region is not None
+        and region not in phonenumbers.shortdata._AVAILABLE_REGION_CODES
+    ):
+        raise ValueError(
+            "`%s` is not a valid region code. Choices are %r"
+            % (region, phonenumbers.shortdata._AVAILABLE_REGION_CODES)
+        )
+
+
+def get_formatted_phone(parsed_value: PhoneNumber, with_country=False, with_hyphen=False) -> str:
+    if parsed_value.is_valid_number():
+        if with_country:
+            # INTERNATIONAL -> +82 10-1234-5678, E164 -> +821086075857
+            attr = "INTERNATIONAL" if with_hyphen else "E164"
+            fmt = getattr(phonenumbers.PhoneNumberFormat, attr)
+        else:
+            # NATIONAL -> 010-1234-5857
+            fmt = phonenumbers.PhoneNumberFormat.NATIONAL
+        value = phonenumbers.format_number(parsed_value, fmt)
+    else:
+        value = parsed_value.raw_input
+
+    if not with_hyphen:
+        value = value.replace("-", "")
+
+    return value
+
+
+def get_phone(value: str, region='KR') -> PhoneNumber:
+    validate_region(region)
+
+    if not value:
+        phone_number = value
+    elif isinstance(value, str):
+        try:
+            phone_number = PhoneNumber(raw_number=value, region=region)
+        except PhoneNumberParseException:
+            phone_number = BasePhoneNumber(raw_input=value)
+    elif isinstance(value, PhoneNumber):
+        phone_number = value
+    elif isinstance(value, phonenumbers.PhoneNumber):
+        phone_number = PhoneNumber()
+        phone_number.merge_from(value)
+    else:
+        raise TypeError("Can't convert %s to PhoneNumber." % type(value).__name__)
+
+    return phone_number
--- server/crud/user.py
+++ server/crud/user.py
@@ -1,4 +1,4 @@
-from server.core.utils import hash_password
+from server.core.utils import hash_password, get_formatted_phone, get_phone
 from server.crud import CRUDBase
 from server.models.user import User, UserSession, UserProfile, UserProfileImage
 
@@ -8,8 +8,9 @@ class UserCRUD(CRUDBase):
 
     async def create(self, **kwargs):
         kwargs.update({
-            "uid": kwargs["email"],
-            "password": hash_password(kwargs["password"])
+            'uid': kwargs['email'],
+            'password': hash_password(kwargs['password']),
+            'mobile': get_formatted_phone(get_phone(kwargs['mobile']), with_country=True)
         })
         user = User(**kwargs)
         self.session.add(user)
