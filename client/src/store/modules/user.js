243979b75a81afaafee4a71817ff5773029b56e9
jonus <positipman@gmail.com>
2022-12-25 22:12:36 +0900

    Update: Vuex 유저 프로필 저장

--- client/src/store/modules/user.js
+++ client/src/store/modules/user.js
@@ -3,9 +3,15 @@ const user = {
     state: {
         userId: '',
         userEmail: '',
-        userMobil: '',
+        userMobile: '',
         userName: '',
         userIsActive: false,
+        profileId: '',
+        profileNickname: '',
+        profileStatusMessage: '',
+        profileImages: [],
+        profileIsDefault: false,
+        profileIsActive: false,
     },
     getters: {
         getUserId(state) {
@@ -14,12 +20,33 @@ const user = {
         getUserEmail(state) {
             return state.userEmail;
         },
+        getUserMobile(state) {
+            return state.userMobile;
+        },
         getUserName(state) {
             return state.userName;
         },
         getUserIsActive(state) {
             return state.userIsActive;
-        }
+        },
+        getProfileId(state) {
+            return state.profileId;
+        },
+        getProfileNickname(state) {
+            return state.profileNickname;
+        },
+        getProfileStatusMessage(state) {
+            return state.profileStatusMessage;
+        },
+        getProfileImages(state) {
+            return state.profileImages;
+        },
+        getProfileIsDefault(state) {
+            return state.profileIsDefault;
+        },
+        getProfileIsActive(state) {
+            return state.profileIsActive;
+        },
     },
     actions: {
         login({ commit }, value) {
@@ -31,18 +58,30 @@ const user = {
     },
     mutations: {
         loginUser(state, value) {
-            state.userId = value.userId
-            state.userEmail = value.userEmail
-            state.userMobile = value.userMobile
-            state.userName = value.userName
-            state.userIsActive = value.userIsActive
+            state.userId = value.userId;
+            state.userEmail = value.userEmail;
+            state.userMobile = value.userMobile;
+            state.userName = value.userName;
+            state.userIsActive = value.userIsActive;
+            state.profileId = value.profileId;
+            state.profileNickname = value.profileNickname;
+            state.profileStatusMessage = value.profileStatusMessage;
+            state.profileImages = value.profileImages;
+            state.profileIsDefault = value.profileIsDefault;
+            state.profileIsActive = value.profileIsActive;
         },
         logoutUser(state) {
-            state.userId = ''
-            state.userEmail = ''
-            state.userMobil = ''
-            state.userName = ''
-            state.userIsActive = false
+            state.userId = '';
+            state.userEmail = '';
+            state.userMobile = '';
+            state.userName = '';
+            state.userIsActive = false;
+            state.profileId = '';
+            state.profileNickname = '';
+            state.profileStatusMessage = '';
+            state.profileImages = [];
+            state.profileIsDefault = false;
+            state.profileIsActive = false;
         }
     }
 }
