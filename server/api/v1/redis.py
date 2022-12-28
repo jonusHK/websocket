a42a182d2d7cc900f258020944640f77132f9b57
jonus <positipman@gmail.com>
2022-12-29 00:24:31 +0900

    Add: redis 친구 리스트 조회 API 추가

--- server/api/v1/redis.py
+++ server/api/v1/redis.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from server.api import ExceptionHandlerRoute
 from server.core.externals.redis import AioRedis
 from server.core.externals.redis.schemas import RedisUserProfilesByRoomS, RedisChatRoomsByUserProfileS, \
-    RedisChatHistoriesByRoomS
+    RedisChatHistoriesByRoomS, RedisFollowingsByUserProfileS
 
 router = APIRouter(route_class=ExceptionHandlerRoute)
 
@@ -23,3 +23,8 @@ async def chat_rooms_by_user_profile(user_profile_id: int):
 @router.get("/chats")
 async def chat_histories_by_room(room_id: int):
     return await RedisChatHistoriesByRoomS.zrange(redis, room_id)
+
+
+@router.get("/followings")
+async def followings_by_user_profile(user_profile_id: int):
+    return await RedisFollowingsByUserProfileS.smembers(redis, user_profile_id)
