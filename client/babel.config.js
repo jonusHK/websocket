8e22b8fdf21f9b37ff3704fb605c7e3be2a2bb11
jonus <positipman@gmail.com>
2023-02-21 14:44:45 +0900

    Update: add babel config

---
+++ client/babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    presets: [
+      ['@babel/preset-env', { targets: 'defaults' }]
+    ],
+    // plugins: [
+    //   ['@babel/plugin-transform-strict-mode', { strict: false }]
+    // ],
+    // exclude: [
+    //     /node_modules\/(?!(jquery)\/)/
+    // ]
+  };
