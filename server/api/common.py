2240fd345f4d7781c6331834653a2c79369d2d13
jonus <positipman@gmail.com>
2023-02-28 22:39:57 +0900

    Add: redis info test

--- server/api/common.py
+++ server/api/common.py
@@ -69,6 +69,7 @@ class RedisHandler:
     async def generate_primary_redis(cls, connections: List[Redis]):
         async for conn in async_iter(connections):
             info = await conn.info()
+            print('redis info role ------ ', info.get('role'))
             if info.get('role') == 'master':
                 return conn
         return
