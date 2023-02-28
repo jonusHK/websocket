c195b217f4f3ed763db2f3762496c6d73fbedfe8
jonus <positipman@gmail.com>
2023-03-01 00:28:38 +0900

    Add: 테스트 로깅

--- server/api/common.py
+++ server/api/common.py
@@ -69,7 +69,9 @@ class RedisHandler:
     async def generate_primary_redis(cls, connections: List[Redis]):
         async for conn in async_iter(connections):
             info = await conn.info()
-            print('redis info role ------ ', info.get('role'))
+            import logging
+            logger = logging.getLogger('websocket')
+            logger.info(f" redis info role : {info.get('role')}")
             if info.get('role') == 'master':
                 return conn
         return
